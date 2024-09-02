d0214579cdc988e439de19e1d1a3a79b4dd99d43
BroAZIK <124960430+BroAZIK@users.noreply.github.com>
2024-09-02 20:46:58 +0500

    Create webhook.py

---
+++ webhook.py
@@ -0,0 +1,18 @@
+from telegram import Bot
+from .settings import *
+
+bot = Bot(token=TOKEN)
+
+def get_info():
+    print(bot.get_webhook_info())
+
+
+def delete():
+    print(bot.delete_webhook())
+
+
+def set():
+    url = 'https://SMMBOTwithAZIZBEK.pythonanywhere.com/webhook'
+    print(bot.set_webhook(url=url))
+
+set()
